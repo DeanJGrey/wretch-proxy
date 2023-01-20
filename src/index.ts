e151038e71868ecb898b3dc2dc87343a18c4aefd
Dean Grey <Dean.J.Grey@icloud.com>
2023-01-20 22:48:03 +1100

    modified:   .gitignore 	modified:   src/index.ts 	modified:   tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {FetchLike, WretchOptions} from "wretch"
 
-export default function proxy (proxyURL: string, proxyOptions: Omit <RequestInit, "body" | "method">)
+export default function proxy ({proxyURL, proxyOptions}: {proxyURL: string, proxyOptions: Omit <RequestInit, "body" | "method">})
 {
     return (next: FetchLike) =>
     {
